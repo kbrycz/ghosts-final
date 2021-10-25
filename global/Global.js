0ed5a0182d441613eb3134c4d40109ea7fcf3f91
kbrycz <kbrycz@umich.edu>
2021-10-25 13:05:02 -0500

    fixing up all styling

--- global/Global.js
+++ global/Global.js
@@ -1,3 +1,3 @@
-// export let server = "http://159.223.109.231:3000"
-export let server = "http://886f-168-91-239-128.ngrok.io"
+export let server = "http://159.223.109.231:3000"
+// export let server = "http://886f-168-91-239-128.ngrok.io"
 export let socket = null
